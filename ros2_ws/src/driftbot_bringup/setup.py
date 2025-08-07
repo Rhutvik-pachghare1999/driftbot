8d133e715adff43cf9e609ef3dcdc729cf094d3f
Rhutvik_pachghare <rpachagha@asu.edu>
2025-08-06 20:00:00 -0700

    Scaffold driftbot_bringup ROS2 (ament_python) package

---
+++ ros2_ws/src/driftbot_bringup/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'driftbot_bringup'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml') + glob('config/*.rviz')),
+        (os.path.join('share', package_name, 'scripts'), glob('scripts/*.sh') + glob('scripts/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'scan_assembler = driftbot_bringup.scan_assembler:main',
+            'odometry_node = driftbot_bringup.odometry_node:main',
+            'topic_monitor = driftbot_bringup.topic_monitor:main',
+        ],
+    },
+)
